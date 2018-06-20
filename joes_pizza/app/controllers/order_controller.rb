0930db43042b9da5311547ecc4228914d497836c
Andres Howard <ahowardm@gmail.com>
2018-06-20 19:24:38 -0400

    advancing

--- joes_pizza/app/controllers/order_controller.rb
+++ joes_pizza/app/controllers/order_controller.rb
@@ -1,6 +1,6 @@
 class OrderController < ApplicationController
-  def show
+  def index
     @orders = Order.where(user_id: params[:user_id])
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
