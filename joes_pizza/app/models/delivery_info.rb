81013816e0f746427ca32a86e1b675b43e23a64d
Andres Howard <ahowardm@gmail.com>
2018-06-20 15:29:02 -0400

    bootstrap

--- joes_pizza/app/models/delivery_info.rb
+++ joes_pizza/app/models/delivery_info.rb
@@ -1,2 +1,6 @@
 class DeliveryInfo < ApplicationRecord
+  validates :address1, presence: true
+  validates :address2, presence: true
+  validates :phone, presence: true
+  validates :phone, numericality: true
 end
